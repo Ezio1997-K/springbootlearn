ea1d63eba0a02e203c639659753a5eb856febfd4
Ezio1997-K <2911019397@qq.com>
2024-12-15 17:09:14 +0800

    脚手架的使用

---
+++ spingboot3-02-mybatis/src/test/java/com/learnspringboot/spingboot3/Spingboot302MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.spingboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spingboot302MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
